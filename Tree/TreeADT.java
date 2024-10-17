9ff0ab35ba20932f122252df303ed819dd759c2d
TrongPhuc <nguyentrongphuc03@gmail.com>
2024-10-17 22:23:02 +0700

    Tree

---
+++ Tree/TreeADT.java
@@ -0,0 +1,13 @@
+package Tree;
+
+import java.util.Iterator;
+
+public interface TreeADT<T> {
+    boolean isEmpty();
+    int size();
+    int height();
+    boolean contains(T elem);
+    boolean add(T elem);
+    boolean remove(T elem);
+    Iterator<T> traverse(TreeTraverseType type);;
+}
